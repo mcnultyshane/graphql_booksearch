35c12d362c1e363b755abfead58f0febb9ee73ad
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-08-06 21:06:59 -0400

    added dotenv added connections and auth and schema.

--- server/server.js
+++ server/server.js
@@ -16,10 +16,20 @@ const server = new ApolloServer({
 server.applyMiddleware({ app });
 
 app.use(express.urlendcoded({ extended: false}));
-
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build'))
     );
 }
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
