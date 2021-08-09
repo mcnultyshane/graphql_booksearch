2fe5297ad94c8afb4b392522258076583ab37ed5
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-08-09 12:15:12 -0400

    typed out muations client side.

--- server/server.js
+++ server/server.js
@@ -4,20 +4,27 @@ const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth')
 
-const PORT = process.env.PORT || 3001;
+// Set up express server
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+// Create a new Apollo server and pass in the schema data 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: authMiddleware
 });
 
+// integrate Apollo server with teh Express apllication as middleware
 server.applyMiddleware({ app });
 
+// Express middleware for parsing
 app.use(express.urlendcoded({ extended: false}));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build'))
     );
